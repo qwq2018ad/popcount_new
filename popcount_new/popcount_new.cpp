fc8f4ad7f38ca8cc5b2ad095ceeff0543ad509e9
qwq2018ad <neil910119@gmail.com>
2024-09-18 23:39:41 +0800

    vector to array

--- popcount_new/popcount_new.cpp
+++ popcount_new/popcount_new.cpp
@@ -75,47 +75,31 @@ int popcount(unsigned long long i) {
 //
 //    return hammingDistance;
 //}
-vector<int> SlidingWindow(unsigned long long* buffer1, int l,int buffer1Sizes) {
+void SlidingWindow(unsigned long long* buffer1, int* result, int l,int buffer1Sizes) {
     int hammingDistance = 0;
     unsigned long long B_WitnessBit, BinvWitnessBit, xorResult;
     int m = ceilLog2_atcgmap_Count; // 幾個0
     int n = Tsize; // 100重複幾次
 
     unsigned long long b1 = buffer1[0];
-    /*bitset<12> b1binary(b1);
-    cout << b1binary << endl;*/
-    std::vector<int> result;
-    /*cout << "Tsize :" << Tsize << endl;
-    cout << "Tsize - l + 1 :" << Tsize - l + 1 << endl;*/
     for (int i = 0; i < Tsize - l + 1; i++) {
         unsigned long long bi = buffer1[i];
-        bitset<24> bibinary(bi);
-        cout << i << " :" << bibinary << endl;
+        /*bitset<24> bibinary(bi);
+        cout << i << " :" << bibinary << endl;*/
         int distance = 0;
         xorResult = b1 ^ bi;
-        /*bitset<18> xorResultbinary(xorResult);
-        cout << "xorResul" << ":" << xorResultbinary << endl;*/
+
         B_WitnessBit = generate_WitnessBit(m, l, i);
-        //bitset<18> B_WitnessBitbinary(B_WitnessBit);
-        //cout << "B_WitnessBitbinary" << ":" << B_WitnessBitbinary << endl;
+
         BinvWitnessBit = ~B_WitnessBit;
-        /*unsigned long long k;
-        k = ((xorResult + BinvWitnessBit) & B_WitnessBit);*/
-        distance = popcount((xorResult + BinvWitnessBit) & B_WitnessBit);
 
-        //bitset<18> kbinary(k);
-        //cout << "kbinary " << ":" << kbinary << endl;
-        //
-        //cout << "distance :" << distance<<endl;
-
-        result.push_back(distance);
+        result[i] = popcount((xorResult + BinvWitnessBit) & B_WitnessBit);
 
     }
-    return result;
 }
 int main() {
     auto s = chrono::high_resolution_clock::now();//計算字串轉換時間
-    l = 4; //4, 8, 16
+    l = 15; //4, 8, 16
     unordered_map<char, unsigned char> atcgmapToBit = {
         {'A', 0},
         {'T', 1},
@@ -133,26 +117,26 @@ int main() {
 
     //string T = "AAAATTTTCCCCGGGG";// 16
 
-    string T = "GAGTCAGAGTA";//11 
-    cout << "T :" << T<<endl;
+    //string T = "GAGTCAGAGTA";//11 
+    //cout << "T :" << T<<endl;
 
     //string T = "GGCC";//4
 
     //檔案名稱
-    //string filename1 = "dna_500M.txt";
-    //ifstream infile1(filename1);       // 以讀取模式打開檔案
+    string filename1 = "dna_1G.txt";
+    ifstream infile1(filename1);       // 以讀取模式打開檔案
 
-    //// 檢查檔案是否成功打開
-    //if (!infile1) {
-    //    cerr << "無法打開檔案 " << filename1 << endl;
-    //    return 1;
-    //}
+    // 檢查檔案是否成功打開
+    if (!infile1) {
+        cerr << "無法打開檔案 " << filename1 << endl;
+        return 1;
+    }
 
-    //stringstream buffer1;
+    stringstream buffer1;
 
-    //buffer1 << infile1.rdbuf();  // 將檔案內容讀入到 stringstream 中
+    buffer1 << infile1.rdbuf();  // 將檔案內容讀入到 stringstream 中
 
-    //string T = buffer1.str();  // 將 stringstream 中的內容轉換為 string
+    string T = buffer1.str();  // 將 stringstream 中的內容轉換為 string
 
     Tsize = T.length();
     //read str to ULL
@@ -167,16 +151,11 @@ int main() {
     encodedBuffer1[0] = 0;
     for (int i = 0; i < l ; i++)
         encodedBuffer1[0] = (encodedBuffer1[0]<<3 |base_lookup(T[i]));
-        /*bitset<15> Buffer1binary(encodedBuffer1[0]);//l*3
-        cout << "Buffer" << 0 << " :" << Buffer1binary << endl;*/
+
     // 對字串進行編碼
     for (int i = 1; i < Tsize -l+1; ++i) {
         unsigned long long baseBit = base_lookup(T[i+l-1]) & 0b111;  // 獲取字符的 3 位編碼
         encodedBuffer1[i] = (encodedBuffer1[i-1]) << 3|baseBit;  // 將 encodedBuffer1 左移
-        //?加入&000 111 111 111
-
-        /*bitset<15> Buffer1binary(encodedBuffer1[i]);//l*3
-        cout << "Buffer"<<i << " :" << Buffer1binary << endl;*/
     }
 
     auto e = chrono::high_resolution_clock::now();//計算字串轉換時間
@@ -184,10 +163,10 @@ int main() {
 
     double at = 0;
     int j = 0;
-    std::vector<int> result;
+    int* result = new int[buffer1Sizes];
     for (j = 0; j < 1; j++) {
         auto start = chrono::high_resolution_clock::now();
-        result = SlidingWindow(encodedBuffer1, l, buffer1Sizes);
+        SlidingWindow(encodedBuffer1,  result, l, buffer1Sizes);
         auto end = chrono::high_resolution_clock::now();
         //輸出結果
         //cout << T << " 的二進制表示: "<<endl;
@@ -196,16 +175,16 @@ int main() {
         //    cout << baseBitbinary << endl;
         //}
 
-        for (int i = 0; i < result.size(); ++i) {// one substring
-            cout << result[i] << " ";
-        }
+        //for (int i = 0; i < buffer1Sizes; ++i) {// one substring
+        //    cout << result[i] << " ";
+        //}
 
-        //std::ofstream outFile("output.txt");
+        std::ofstream outFile("output.txt");
 
-        //// 檢查文件是否成功打開
+        // 檢查文件是否成功打開
         //if (outFile.is_open()) {
         //    // 將陣列的每個元素寫入文件，並用空格分隔
-        //    for (int i = 0; i < result.size(); ++i) {
+        //    for (int i = 0; i < buffer1Sizes; ++i) {
         //        outFile << result[i];
         //    }
         //    outFile.close(); // 完成後關閉文件
